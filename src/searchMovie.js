27e00a49c35b27a4d08a7874a47f2861d8ac1f73
Eduardo PL <mini0n.000@gmail.com>
2020-09-10 17:54:29 -0500

    searchMovies() 1st

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 export default function SearchMovie() {
+  const key = process.env.REACT_APP_TMDB_API_KEY;
+
+  const srch = "/search/movie";
+  const lang = "language-en-US";
+  const query = "Midsommar";
+
+  const url = `https://api.themoviedb.org/3${srch}?api_key=${key}&${lang}&query=${query}`;
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("Searching...");
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Search a Movie:
       </label>
