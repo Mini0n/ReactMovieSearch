86e9003a39c11c263e2514a3305a6e685fc3b81c
Eduardo PL <mini0n.000@gmail.com>
2020-09-10 00:49:32 -0500

    display search movie

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+import SearchMovie from "./searchMovie";
+
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
         <h3>Ola k ase!</h3>
+        <SearchMovie />
       </div>
     );
   }
